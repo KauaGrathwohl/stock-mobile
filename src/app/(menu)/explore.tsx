39a108a69cb31859f4be5dbb934692edd30a79e0
gmDarabas <gmd.darabas@hotmail.com>
2024-11-17 19:44:20 -0300

    add drawer menu

---
+++ src/app/(menu)/explore.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TabTwoScreen() {
+  return <h2 style={{ textAlign: "center" }}>Explore page</h2>;
+}
---
+++ src/app/(menu)/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HomeScreen() {
+  return <h2 style={{ textAlign: "center" }}>Index Page</h2>;
+}
