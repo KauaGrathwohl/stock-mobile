7dc989ce9c637a9c3f32cb39b472f2eac39e7db3
gus <dev@ninsaude.com>
2024-11-24 12:39:50 -0300

    fix no authprovider

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,5 +1,5 @@
 import { ReactNode, useState, useEffect } from "react";
-import { Auth, Empresa, EmpresaResponse, User, UserResponse } from "../interfaces/api";
+import { Auth, Empresa, User, UserResponse } from "../interfaces/api";
 import { useFetch } from "../hooks/useFetch";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 import { AuthContext } from "../contexts/AuthContext";
@@ -9,7 +9,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const [user, setUser] = useState<User | null>(null);
     const [empresa, setEmpresa] = useState<Empresa | null>(null);
 
-    const [responseEmpresa, fetchDataEmpresa] = useFetch<{company: Empresa}>();
+    const [responseEmpresa, fetchDataEmpresa] = useFetch<{ company: Empresa }>();
     const [responseUser, fetchDataUser] = useFetch<UserResponse>();
 
     const { setItem, getItem, removeItem } = useLocalStorage();
@@ -18,9 +18,8 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
         const token = await getItem('token');
         const empresa = await getItem('empresa');
         const user = await getItem('user');
-        
+
         if (token && empresa && user) {
-            setIsAuthenticated(true);
             return {
                 menssage: '',
                 token,
@@ -34,75 +33,84 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const [dataLogin, setDataLogin] = useState<Auth | null>(null);
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchStoredData = async () => {
             const storedData = await getLoginByLocalStorage();
             if (storedData) {
                 setDataLogin(storedData);
+                setIsAuthenticated(true);
             }
         };
-    
-        fetchData();
+        fetchStoredData();
     }, []);
 
-    const login = (data: Auth) => {
-        setItem('token', data.token);
-        setItem('empresa', data.empresa.toString());
-        setItem('user', data.usuario.toString());
+    const login = async (data: Auth) => {
+        await setItem('token', data.token);
+        await setItem('empresa', data.empresa.toString());
+        await setItem('user', data.usuario.toString());
         setDataLogin(data);
         setIsAuthenticated(true);
     };
 
-    const logout = () => {
-        removeItem('token');
-        removeItem('empresa');
-        removeItem('user');
+    const logout = async () => {
+        resetStates();
+        await removeItem('token');
+        await removeItem('empresa');
+        await removeItem('user');
+    };
+
+    const resetStates = () => {
         setDataLogin(null);
         setIsAuthenticated(false);
+        setUser(null);
+        setEmpresa(null);
     };
 
     const getEmpresaData = async () => {
         if (dataLogin) {
-            const url = `${process.env.EXPO_PUBLIC_API_URL}/empresa/${dataLogin.empresa}?empresa=${dataLogin.empresa}`; 
-            const headers = { 
+            const url = `${process.env.EXPO_PUBLIC_API_URL}/empresa/${dataLogin.empresa}?empresa=${dataLogin.empresa}`;
+            const headers = {
                 'Authorization': `Bearer ${dataLogin.token}`,
                 'Content-Type': 'application/json'
             };
-            const method = 'GET';
-            await fetchDataEmpresa(url, { headers, method });
-            if (responseEmpresa.data) {
-                setEmpresa(responseEmpresa.data.company);
-            }
+            await fetchDataEmpresa(url, { headers, method: 'GET' });
         }
     };
 
     const getUserData = async () => {
         if (dataLogin) {
-            const url = `${process.env.EXPO_PUBLIC_API_URL}/usuario/${dataLogin.usuario}?empresa=${dataLogin.empresa}`; 
+            const url = `${process.env.EXPO_PUBLIC_API_URL}/usuario/${dataLogin.usuario}?empresa=${dataLogin.empresa}`;
             const headers = {
-                'Authorization': `Bearer ${dataLogin.token}`, 
+                'Authorization': `Bearer ${dataLogin.token}`,
                 'Content-Type': 'application/json'
             };
-            const method = 'GET';
-            await fetchDataUser(url, { headers, method });
-            if (responseUser.data && dataLogin.usuario) {
-                setUser(responseUser.data.user);
-            }
+            await fetchDataUser(url, { headers, method: 'GET' });
         }
     };
 
     useEffect(() => {
-        if(responseEmpresa.error || responseUser.error) logout();
         if (dataLogin && isAuthenticated) {
             getEmpresaData();
             getUserData();
-            return 
         }
-        logout();
-    }, [dataLogin, isAuthenticated, !responseEmpresa.data, !responseUser.data, responseEmpresa.error, responseUser.error]);
+    }, [dataLogin, isAuthenticated]);
+
+    useEffect(() => {
+        if (responseEmpresa.error || responseUser.error) {
+            logout();
+        }
+
+        if (responseEmpresa.data) {
+            setEmpresa(responseEmpresa.data.company);
+        }
+
+        if (responseUser.data) {
+            setUser(responseUser.data.user);
+        }
+    }, [responseEmpresa, responseUser]);
 
     return (
-      <AuthContext.Provider value={{ isAuthenticated, login, logout, dataLogin, user, empresa }}>
-        {children}
-      </AuthContext.Provider>
+        <AuthContext.Provider value={{ isAuthenticated, login, logout, dataLogin, user, empresa }}>
+            {children}
+        </AuthContext.Provider>
     );
 };
